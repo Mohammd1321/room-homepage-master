5fc6b4dfaa9a3e30473d21843f5c8557f9b78012
Mohammd1321 <69431624+Mohammd1321@users.noreply.github.com>
2021-06-08 19:32:33 +0300

    room-files

--- main.js
+++ main.js
@@ -1,17 +1,59 @@
 'use strict';
 const slide = document.querySelector('.slide');
 const allImgs = document.querySelectorAll('.slide img');
-const buttons = document.querySelectorAll('button');
 const nextButton = document.querySelector('.next');
 const previousButton = document.querySelector('.previous');
-const end = document.querySelector('.end');
 const closes = document.querySelector('.close');
 const opens = document.querySelector('.open');
 const navBar = document.querySelector('.menu');
 const overlay = document.querySelector('.overlay');
+const text = document.querySelector('.text');
+
+
+const element = document.querySelector('.s');
+
+
+const words = [
+  {
+    heading: "Manufactured with the best materials",
+    description: "Our modern furniture store provide a high level of quality. Our company has invested in advanced technology to ensure that every product is made as perfect and as consistent as possible. With three decades of experience in this industry, we understand what customers want for their home and office."
+  },
+  {
+    heading: "Discover innovative ways to decorate",
+    description: "we provide unmatched quality comfort. style for property owners across the country our experts combine form and function in bringing your vision to life. Create a room with your own style with our collection and make your property a reflection of you and what you love.",
+  },
+  {
+    heading: "We are available all across the globe",
+    description: "With stores all over the world, it's easy for you to find furniture for your home or place of business. Locally, we’re in most major cities throughout the country. Find the branch nearest you using our store locator. Any questions? Don't hesitate to contact us today."
+  },
+  {
+    heading: "Manufactured with the best materials",
+    description: "Our modern furniture store provide a high level of quality. Our company has invested in advanced technology to ensure that every product is made as perfect and as consistent as possible. With three decades of experience in this industry, we understand what customers want for their home and office."
+  },
+  {
+    heading: "Discover innovative ways to decorate",
+    description: "we provide unmatched quality comfort. style for property owners across the country our experts combine form and function in bringing your vision to life. Create a room with your own style with our collection and make your property a reflection of you and what you love.",
+  },
+]
+
+
+
+
 
 let counter = 1;
 let size = slide.clientWidth;
+
+window.addEventListener('DOMContentLoaded',function() {
+  texts();
+  slider();
+})
+
+
+window.addEventListener('resize',debounce(e => {
+  size = slide.clientWidth;
+  slide.style.transform = 'translateX(' + (-size * counter) + 'px)';
+},1000))
+
 
 
 
@@ -24,25 +66,40 @@ opens.addEventListener('click',function() {
   overlay.style.display = 'block';
 })
 
-slider();
 
 
 nextButton.addEventListener('click',function() {
   counter++;
+  texts();
   slider();
 })
 previousButton.addEventListener('click',function() {
   counter--;
+  texts();  
   slider();
 })
 
 slide.addEventListener('transitionend',function() {
   swipe('end',1);
+  texts();
 })
 
 slide.addEventListener('transitionend',function() {
   swipe('start',allImgs.length -2);
 })
+
+
+
+
+
+
+
+function texts() {
+  text.innerHTML= `
+  <h1>${words[counter].heading}</h1>
+  <h3>${words[counter].description}</h3>
+  <button type="button">SHOP NOW <svg width="40" height="12" xmlns="http://www.w3.org/2000/svg"><path d="M34.05 0l5.481 5.527h.008v.008L40 6l-.461.465v.063l-.062-.001L34.049 12l-.662-.668 4.765-4.805H0v-1h38.206l-4.82-4.86L34.05 0z" fill="#000" fill-rule="nonzero"/></svg></button>`
+}
 
 
 function slider() {
@@ -55,5 +112,18 @@ function swipe(text,number) {
     counter = number;
     slide.style.transition = 'none';
     slide.style.transform = 'translateX(' + (-size * counter) + 'px)';
+  }
+}
+
+
+function debounce(fn,delay) {
+  let timeout;
+  return function() {
+    if(timeout) {
+      clearTimeout(timeout);
+    }
+    timeout = setTimeout(() => {
+      fn();
+    },delay)
   }
 }
